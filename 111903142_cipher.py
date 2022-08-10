24649366496fc06c0b9c5e514daffbbb62e7a4ca
Dhruva-Kapadia <69617109+Dhruva-Kapadia@users.noreply.github.com>
2022-08-10 11:58:55 +0530

    Update 111903142_cipher.py

--- 111903142_cipher.py
+++ 111903142_cipher.py
@@ -1,27 +1,33 @@
-file1 = open('plaintext.txt', 'r')
-a = file1.read()
+#a = "Hello World"
+#e = "HFNOS XQUPD"
 
-file2 = open('encryptedtext.txt', 'w')
-a = "Hello World"
-e = "HFNOS XQUPD"
+file3 = open('outputtext.txt', 'a')
 
-key = int(input("Enter key:\n"))
-
-def encrypt(plaintext):    
+def encrypt(plaintext, key, file):    
     index = 0
     b = ""
     for i in plaintext:
         b += chr(ord(i) + (index % key))
         index+=1
-    file2.write(b.upper())
+    file.write(b.upper())
 
-def decrypt(encryptedtext):
+def decrypt(encryptedtext, key, file):
     index = 0
     b = ""
     for i in encryptedtext:
         b += chr(ord(i) - (index % key))
         index+=1
-    file2.write(b.lower())
-    
-encrypt(a)
-decrypt(e)
+    file.write(b.lower())
+
+o = int(input("1. Encryption\n2. Decryption\nEnter Choice:\n"))
+key = int(input("Enter key:\n"))
+if(o == 1):
+    file1 = open('plaintext.txt', 'r')
+    p = file1.read()
+    encrypt(p, key, file3)
+elif(o == 2):
+    file2 = open('encryptedtext.txt', 'w')
+    e = file2.read()
+    decrypt(e, key, file3)
+else:
+    print("invalid input")
