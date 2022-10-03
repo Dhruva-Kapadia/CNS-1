53a83d177c82c0e7730b3c105b3709c09d212893
Dhruva-Kapadia <69617109+Dhruva-Kapadia@users.noreply.github.com>
2022-10-03 10:41:34 +0530

    Update 111903142_cipher.py

--- 111903142_cipher.py
+++ 111903142_cipher.py
@@ -3,31 +3,31 @@
 
 file3 = open('outputtext.txt', 'a')
 
-def encrypt(plaintext, key, file):    
+def encrypt(plaintext, key):    
     index = 0
     b = ""
     for i in plaintext:
         b += chr(ord(i) + (index % key))
         index+=1
-    file.write(b.upper())
+    file3.write(b.upper())
 
-def decrypt(encryptedtext, key, file):
+def decrypt(encryptedtext, key):
     index = 0
     b = ""
     for i in encryptedtext:
         b += chr(ord(i) - (index % key))
         index+=1
-    file.write(b.lower())
+    file3.write(b.lower())
 
 o = int(input("1. Encryption\n2. Decryption\nEnter Choice:\n"))
 key = int(input("Enter key:\n"))
 if(o == 1):
     file1 = open('plaintext.txt', 'r')
     p = file1.read()
-    encrypt(p, key, file3)
+    encrypt(p, key)
 elif(o == 2):
     file2 = open('encryptedtext.txt', 'w')
     e = file2.read()
-    decrypt(e, key, file3)
+    decrypt(e, key)
 else:
     print("invalid input")
